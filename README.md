1e6ecba44ad36e7390c9e428bcaf20e9521b0326
key-amb <yasutake.kiyoshi@gmail.com>
2014-12-23 04:35:16 +0900

    created by minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+AWS::CLI::Config - It's new $module
+
+# SYNOPSIS
+
+    use AWS::CLI::Config;
+
+# DESCRIPTION
+
+AWS::CLI::Config is ...
+
+# LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+YASUTAKE Kiyoshi <yasutake.kiyoshi@gmail.com>
---
+++ lib/AWS/CLI/Config.pm
@@ -0,0 +1,39 @@
+package AWS::CLI::Config;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+AWS::CLI::Config - It's new $module
+
+=head1 SYNOPSIS
+
+    use AWS::CLI::Config;
+
+=head1 DESCRIPTION
+
+AWS::CLI::Config is ...
+
+=head1 LICENSE
+
+Copyright (C) YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+YASUTAKE Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=cut
+
